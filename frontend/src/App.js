03b9fd1553de682e706eacb95ed8f3fa98f08077
Saathi Paul <saathipaul@gmail.com>
2024-09-20 23:21:32 +0530

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,9 @@
 import React from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 5dfa8bcb369e72c589c59e3b1ef5a67283022cad
 function App() {
   return (
-
-<Layout/>
-
-
-
+   <Layout/>
   )
-
 }
-
 export default App;
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,58 @@
+// import React from 'react'
+// import './footer.css'
+// import{Container , Row , Col , ListGroup , ListGroupItem } from 'reactstrap';
+
+// import {Link} from 'react-router-dom';
+// import logo from '../../assets/images/logo2.png';
+
+// const quick_links =[
+//   {
+//     path: "/gallery",
+//     display: 'Gallery',
+//   },
+//   {
+//     path:'/login',
+//     display:'Login',
+//   },
+//   {
+//     path:'/register',
+//     display:'Register',
+//   },
+// ]
+
+// const Footer = () => {
+//   return (
+//     <footer className='footer'>
+//       <Container>
+//         <Row>
+//           <Col lg='3'></Col>
+//             <div className='logo'>
+//             <img src={logo} alt=''/>
+//             <p>lorem</p>
+
+//             <div className='social_links d-flex align-items-center gap-4'>
+//             <span>
+//               <Link to='#'><i class='ri-youtube-line'></i></Link>
+//             </span>
+
+//             </div>
+//             </div>
+
+//         </Row>
+//       </Container>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
+
+
 import React from 'react'
 
 const Footer = () => {
   return (
     <div>
-    Saathi1234567
+      
     </div>
   )
 }
