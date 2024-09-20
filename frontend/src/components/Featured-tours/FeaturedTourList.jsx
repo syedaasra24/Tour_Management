cb664fd19256ff39f554b1ea21747627937767ba
syedaasra24 <153835756+syedaasra24@users.noreply.github.com>
2024-09-20 05:35:37 +0530

    3rd commit

--- frontend/src/components/Featured-tours/FeaturedTourList.jsx
+++ frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -1,6 +1,3 @@
-
-
-
 import React from 'react'
 import TourCard from '../../shared/TourCard'
 import tourData from '../../assets/data/tours'
@@ -8,15 +5,19 @@ import { Col } from 'reactstrap'
 
 
 const FeaturedTourList = () => {
-    const tours = _assets
+
+  const data = tourData;
+
   return (
-  <>
-    {tourData?.map(tour=>(
-            <Col lg='3' className='mb-4' key={tour.id}>
-            <TourCard tour={tour} />
-            </Col>
-        ))}
-    </>
+  <div>
+    {
+      data?.map(tour => (
+        <Col lg="3" className='mb-4' key={tour.id}>
+          <TourCard tour={tour}/>
+        </Col>
+      ))
+    }
+  </div>
   );
   
 };
