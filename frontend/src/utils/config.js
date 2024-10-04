ca21829e2a1edce7c6f187d6ed835f83b0f953e2
0888888888 <anshuraj9905151156@gmail.com>
2024-10-04 11:39:16 +0530

    hs

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-export const BASE_URL='http://localhost:4000/api/v1'
-=======
-// export const BASE_URL=''
->>>>>>> 588b6bc461b3d73799f38729592af5280ed3b5af
+export const BASE_URL='http://localhost:4000/api/v1';
+
