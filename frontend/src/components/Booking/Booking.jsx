bc0585c87994a09c436ce4af6af2fa2c35ffd442
Saathi Paul <saathipaul@gmail.com>
2024-10-17 21:14:39 +0530

    TourDetails.jsx

--- frontend/src/components/Booking/Booking.jsx
+++ frontend/src/components/Booking/Booking.jsx
@@ -33,28 +33,30 @@ const Booking = ({ tour, avgRating }) => {
     }
 
   return (
-    <div className= "booking"> 
-       <div className= "booking_top.d-flex.align-items-center.justify-content-between">
-              <h3>${price} <span>/per person</span></h3>
+    <div className= 'booking'>
+       <div className= 'booking_top d-flex align-items-center justify-content-between'>
+              <h3>
+                ${price} <span>/per person</span>
+              </h3>
               <span className="tour_rating d-flex align-items-center ">
-                <i
-                  class="ri-star-s-fill"
-                  ></i>
-                  {avgRating === 0 ? null : avgRating} ( {reviews ?.length})
+                <i class='ri-star-s-fill'></i>
+                {avgRating === 0 ? null : avgRating} ( {reviews ?.length})
             
               </span>
        </div>
 
-
+        {/* ================ Booking Form =======*/}
        <div className="booking_form">
         <h5>Information</h5>
         <From className="booking_info-form" onSubmit={handleClick}>
             <FromGroup>
               <input type="text" placeholder="Full Name" id="fullName" required onChange={handleChange} />
             </FromGroup>
+
             <FromGroup>
               <input type="number" placeholder="Phone" id="phone" required onChange={handleChange} />
             </FromGroup>
+            
             <FromGroup className="d-flex align-items-center gap-3">
               <input type="date" placeholder="" id="bookAt" required onChange={handleChange} />
               <input type="number" placeholder="Guest" id="guestSize" required onChange={handleChange} />
