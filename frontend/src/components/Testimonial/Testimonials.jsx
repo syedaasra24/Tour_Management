17ede48071a14319d86853021048db8bc9ca94d2
0888888888 <anshuraj9905151156@gmail.com>
2024-12-10 16:20:57 +0530

    uyt

--- frontend/src/components/Testimonial/Testimonials.jsx
+++ frontend/src/components/Testimonial/Testimonials.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Slider from 'react-stick';
+import Slider from 'react-slick';
 import ava01 from '../../assets/images/ava-1.jpg';
 import ava02 from '../../assets/images/ava-2.jpg';
 import ava03 from '../../assets/images/ava-3.jpg';
